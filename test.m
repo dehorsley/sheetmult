b6d19a62d618d27ec441d886d6f24c0af39e7338
David Horsley <widdma@gmail.com>
2014-02-18 14:22:45 +1100

    Added test

---
+++ test.m
@@ -0,0 +1,21 @@
+mex sheetmulti.c
+
+MN = 101*100;
+Kth = 201;
+Kr = 102;
+
+R = randn(MN,Kth,Kr);
+A = randn(MN,1);
+
+tic
+P1 = zeros(Kth,Kr);
+for i=1:Kr
+    P1(:,i) = R(:,:,i)'*A; 
+end
+toc
+
+tic
+P2 = sheetmulti(A,R);
+toc
+
+norm(P1-P2)
